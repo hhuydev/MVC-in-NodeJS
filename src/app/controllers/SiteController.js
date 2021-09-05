84b912c53afe152f744843272a3f742e99027ca5
hhuydev <hahuy.dev@gmail.com>
2021-09-05 21:16:24 +0700

    Tao model, db, sua lai get news, site route

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,20 @@
+const Course = require("../models/Course");
 class SiteController {
-    /**GET / */
-    index(req, res) {
-        res.render('home');
-    }
+  /**GET / */
+  index(req, res) {
+    // res.render('home');
+    Course.find({}, function (err, courses) {
+      if (!err) {
+        res.json(courses);
+      } else {
+        res.status(400).json({ error: "ERROR!!!" });
+      }
+    });
+  }
 
-    /**GET /search*/
-    search(req, res) {
-        res.render('search');
-    }
+  /**GET /search*/
+  search(req, res) {
+    res.render("search");
+  }
 }
 module.exports = new SiteController();
