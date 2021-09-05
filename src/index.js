d28f324ebdf23be100952efe235a4fd0153e696a
hhuydev <hahuy.dev@gmail.com>
2021-09-05 14:53:11 +0700

    To chuc folder theo MVC

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const route = require("./routes/index.route");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+/**Dung middleware de lay du lieu submit form */
+app.use(express.urlencoded({ extended: true }));
+/**Dung middleware chuyen thanh json khi gui du lieu len server */
+app.use(express.json());
+
+/**Config file static */
+app.use(express.static(path.join(__dirname, "public")));
+
+/**HTTP logger */
+app.use(morgan("combined"));
+
+/**HandelBars Engine Template */
+app.engine("hbs", handlebars({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+/**Goi den file index chua cac route */
+route(app);
+
+app.listen(port, () => console.log("Server is up on " + port));
